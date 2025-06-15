1decf9ca50a10319d3878100aca34aa78265dbdf
MihaiCatalinVoicu <mihai.catalin_voicu@yahoo.com>
2025-06-15 21:05:32 +0300

    first commit

---
+++ Mini-Library/src/main/java/com/dev/MiniLibrary/entity/Author.java
@@ -0,0 +1,34 @@
+package com.dev.MiniLibrary.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "author")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "First name is required")
+    @Size(min = 3, max = 100, message = "First name must be between 3 and 100 characters")
+    private String firstName;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Last name is required")
+    @Size(min = 3, max = 100, message = "Last name must be between 3 and 100 characters")
+    private String lastName;
+
+    @Column()
+    @Size(min = 3, max = 20, message = "Nationality must be between 3 and 20 characters")
+    private String nationality;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Book> Books;
+}
